7117372c1de50f48e2312b42439dc18abdd71a25
dan-kwiat <dan.kwiat@gmail.com>
2018-12-28 11:36:12 +0000

    Updates

---
+++ precache-manifest.c0f16cc1cada8cef2618b37fbca6e6a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "292be611f00ee47378aa",
+    "url": "/material-react-template/static/js/runtime~main.292be611.js"
+  },
+  {
+    "revision": "51193ae96852a7557689",
+    "url": "/material-react-template/static/js/main.51193ae9.chunk.js"
+  },
+  {
+    "revision": "392a67d97da35abc91b9",
+    "url": "/material-react-template/static/js/1.392a67d9.chunk.js"
+  },
+  {
+    "revision": "51193ae96852a7557689",
+    "url": "/material-react-template/static/css/main.fbf43ca3.chunk.css"
+  },
+  {
+    "revision": "9dce9fa27d90eefd97055850048dabd5",
+    "url": "/material-react-template/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/material-react-template/precache-manifest.684a77fd1ad69f867e655815a7d08cbb.js"
+  "/material-react-template/precache-manifest.c0f16cc1cada8cef2618b37fbca6e6a7.js"
 );
 
 workbox.clientsClaim();
